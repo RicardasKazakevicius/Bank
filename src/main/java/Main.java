b9aedbce01290362630243e75918e7489dc85924
root <ricardaskazak@gmail.com>
2017-04-21 08:31:52 +0300

    added method name to header

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,66 +14,79 @@ public class Main {
         path("/accounts", () -> {
            
             get("", (req, res) -> {
-                res.header("PATH", req.pathInfo());
+                res.header("Path", req.pathInfo());
+                res.header("Method", req.requestMethod());
                 return AccountController.getAllAccounts(req, res, aData);
                 
             }, new JsonTransformer());
             
             get("/:id", (req, res) -> {
-                res.header("PATH", req.pathInfo());
+                res.header("Path", req.pathInfo());
+                res.header("Method", req.requestMethod());
                 return AccountController.getAccount(req, res, aData);
             }, new JsonTransformer());
             
             get("/:id/sent", (req, res) -> {
-                res.header("PATH", req.pathInfo());
+                res.header("Path", req.pathInfo());
+                res.header("Method", req.requestMethod());
                 return AccountController.getSentTransactions(req, res, aData, tData);
             }, new JsonTransformer());
              
             get("/:id/received", (req, res) -> {
-                res.header("PATH", req.pathInfo());
+                res.header("Path", req.pathInfo());
+                res.header("Method", req.requestMethod());
                 return AccountController.getReceivedTransactions(req, res, aData, tData);
             }, new JsonTransformer());
             
             post("", (req, res) -> {
-                res.header("PATH", req.pathInfo() + "/" + (aData.getAll().size()+1));
+                res.header("Path", req.pathInfo() + "/" + (aData.getAll().size()+1));
+                res.header("Method", req.requestMethod());
                 return AccountController.createAccount(req, res, aData);
             }, new JsonTransformer());
             
             put("/:id", (req, res) -> {
-                res.header("PATH", req.pathInfo());
+                res.header("Path", req.pathInfo());
+                res.header("Method", req.requestMethod());
                 return AccountController.updateAccount(req, res, aData);
             }, new JsonTransformer());
             
             delete("/:id", (req, res) -> {
-                res.header("PATH", req.pathInfo());
+                res.header("Path", req.pathInfo());
+                res.header("Method", req.requestMethod());
                 return AccountController.deleteAccount(req, res, aData);
             }, new JsonTransformer());
+            
         });
         
         path("/transactions", () -> {
             
            get("", (req, res) -> {
                res.header("PATH", req.pathInfo());
+               res.header("Method", req.requestMethod());
                return TransactionController.getAllTransactions(req, res, tData);
            }, new JsonTransformer()); 
            
            get("/:id", (req, res) -> {
                res.header("PATH", req.pathInfo());
+               res.header("Method", req.requestMethod());
                return TransactionController.getTransaction(req, res, tData);
            }, new JsonTransformer()); 
            
            post("", (req, res) -> {
                res.header("PATH", req.pathInfo() + "/" + (tData.getAll().size()+1));
+               res.header("Method", req.requestMethod());
                return TransactionController.createTransaction(req, res, tData, aData);
            }, new JsonTransformer()); 
            
            put("/:id", (req, res) -> {
                res.header("PATH", req.pathInfo());
+               res.header("Method", req.requestMethod());
                return TransactionController.updateTransactiont(req, res, tData, aData);
            }, new JsonTransformer()); 
            
            delete("/:id", (req, res) -> {
                res.header("PATH", req.pathInfo());
+               res.header("Method", req.requestMethod());
                return TransactionController.deleteTransaction(req, res, tData);
            }, new JsonTransformer()); 
         });
